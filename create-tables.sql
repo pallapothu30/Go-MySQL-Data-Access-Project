f9d21f541a43cb7d8f9ace81131a4b90bb440dad
Manikanta Pallapothu <pallapothum784@gmail.com>
2025-12-04 20:23:09 +0000

    done with basics

---
+++ create-tables.sql
@@ -0,0 +1,18 @@
+-- Drop the table if it already exists
+DROP TABLE IF EXISTS album;
+
+-- Create the album table
+CREATE TABLE album (
+  id         INT AUTO_INCREMENT NOT NULL,
+  title      VARCHAR(128) NOT NULL,
+  artist     VARCHAR(255) NOT NULL,
+  price      DECIMAL(5,2) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- Insert sample records
+INSERT INTO album (title, artist, price) VALUES
+  ('Blue Train', 'John Coltrane', 56.99),
+  ('Giant Steps', 'John Coltrane', 63.99),
+  ('Jeru', 'Gerry Mulligan', 17.99),
+  ('Sarah Vaughan', 'Sarah Vaughan', 34.98);
